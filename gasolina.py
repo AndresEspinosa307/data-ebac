6cd7cd9b888b925cd58114cc6f03c347e6800fa1
AndresEspinosa307 <theaceofspades307@gmail.com>
2025-03-17 22:53:04 +0000

    adicionando arquivos

---
+++ gasolina.py
@@ -0,0 +1,8 @@
+# código de geração do gráfico
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('gasolina.csv')
+sns.lineplot(data=df, x='dia', y='venda')
+plt.savefig('gasolina.png')
